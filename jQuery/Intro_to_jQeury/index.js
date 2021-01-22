047fedd37d9e1f25df2a0179b69a060fb1ae29de
SofieVonge <43268823+SofieVonge@users.noreply.github.com>
2021-01-22 10:58:17 +0100

    Delete jQuery directory

--- jQuery/Intro_to_jQeury/index.js
+++
@@ -1,8 +0,0 @@
-//får fat i teleportknappen via id'et
-$("#teleport-btn").click(() =>{
-    //får fat i inputtet til venstre og sætter inputtet til højre lig med venstre og omvendt
-    const left = $(".input-left").val()
-    const right = $(".input-right").val()
-    $(".input-right").val(left);
-    $(".input-left").val(right);
-});
